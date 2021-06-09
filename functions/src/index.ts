cc8d7128ce7d0f37e80e94aee868173b7e0ffa1e
Scott Laing <fifersheep@gmail.com>
2021-06-09 22:47:57 +0100

    CI/CD attempt 3

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -7,3 +7,5 @@ export const helloWorld = functions.https.onRequest((request, response) => {
   functions.logger.info("Hello logs!", {structuredData: true});
   response.send("Hello from Firebase!");
 });
+
+// test CI/CD
