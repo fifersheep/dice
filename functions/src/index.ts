8dd7889594d6ff4127c88338b6df75f983815382
Scott Laing <fifersheep@gmail.com>
2021-06-17 00:40:58 +0100

    eslint fix

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -4,48 +4,48 @@ import firebase from "firebase-admin";
 firebase.initializeApp();
 
 export const joinGame = functions.firestore
-  .document("participants/{docId}")
-  .onCreate(async (change, _) => {
-    const data = change.data();
+    .document("participants/{docId}")
+    .onCreate(async (change, _) => {
+      const data = change.data();
 
-    const preExistingDocs = firebase
-      .firestore()
-      .collection("participants")
-      .where("playerId", "==", data["playerId"])
-      .where("gameId", "==", data["gameId"])
-      .get();
+      const preExistingDocs = firebase
+          .firestore()
+          .collection("participants")
+          .where("playerId", "==", data["playerId"])
+          .where("gameId", "==", data["gameId"])
+          .get();
 
-    const participants = (await preExistingDocs).docs;
-    const alreadyCreated = participants.length > 1;
+      const participants = (await preExistingDocs).docs;
+      const alreadyCreated = participants.length > 1;
 
-    if (alreadyCreated) {
-      return change.ref.delete();
-    } else {
-      return Promise.resolve();
-    }
-  });
+      if (alreadyCreated) {
+        return change.ref.delete();
+      } else {
+        return Promise.resolve();
+      }
+    });
 
 export const startGame = functions.firestore
-  .document("participants/{docId}")
-  .onUpdate(async (change, _) => {
-    const gameId = change.after.data()["gameId"];
-    const participants = await firebase
-      .firestore()
-      .collection("participants")
-      .where("gameId", "==", gameId)
-      .get();
+    .document("participants/{docId}")
+    .onUpdate(async (change, _) => {
+      const gameId = change.after.data()["gameId"];
+      const participants = await firebase
+          .firestore()
+          .collection("participants")
+          .where("gameId", "==", gameId)
+          .get();
 
-    const allParticipantsReady = participants.docs.every(
-      (participant) => participant.data()["ready"]
-    );
+      const allParticipantsReady = participants.docs.every(
+          (participant) => participant.data()["ready"]
+      );
 
-    const enoughPlayers = participants.size > 1;
+      const enoughPlayers = participants.size > 1;
 
-    if (allParticipantsReady && enoughPlayers) {
-      return firebase.firestore().collection("games").doc(gameId).update({
-        status: "Started",
-      });
-    } else {
-      return Promise.resolve();
-    }
-  });
+      if (allParticipantsReady && enoughPlayers) {
+        return firebase.firestore().collection("games").doc(gameId).update({
+          status: "Started",
+        });
+      } else {
+        return Promise.resolve();
+      }
+    });
