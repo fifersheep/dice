2d308d014a1c1fdec2617971be1ffc72fc5d2ac8
Scott Laing <fifersheep@gmail.com>
2022-04-20 23:31:54 +0100

    update set_player_ready function to wait for enough players

--- supabase/set_player_ready.sql
+++ supabase/set_player_ready.sql
@@ -2,14 +2,23 @@ create or replace function set_player_ready(player_id bigint, game_id bigint, pl
 returns void
 language plpgsql
 as $$
-declare 
-  _count integer;
+declare
+  _enough_participants boolean := false;
+  _all_players_ready boolean := false;
 begin
   update participations
   set player_ready = set_player_ready.player_ready
   where participations.player_id = set_player_ready.player_id and participations.game_id = set_player_ready.game_id;
 
-  if not exists (select from participations where participations.player_ready = false) then
+  select count(*) > 1
+  into _enough_participants
+  from participations
+  where participations.player_ready = true
+  and participations.game_id = set_player_ready.game_id;
+
+  _all_players_ready := not exists (select from participations where participations.player_ready = false);
+  
+  if (_enough_participants and _all_players_ready) then
     update games
     set status = 'Started'
     where id = set_player_ready.game_id;
