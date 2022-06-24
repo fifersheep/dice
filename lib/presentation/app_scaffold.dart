455dac13710f0133d1f44783fd21b45a109e6b41
Scott Laing <fifersheep@gmail.com>
2022-06-24 17:35:13 +0100

    display version code in bottom left

--- lib/presentation/app_scaffold.dart
+++ lib/presentation/app_scaffold.dart
@@ -1,5 +1,6 @@
 import 'package:dice/presentation/constants/colors.dart';
 import 'package:flutter/material.dart';
+import 'package:package_info_plus/package_info_plus.dart';
 
 class AppScaffold extends StatelessWidget {
   const AppScaffold({Key? key, required this.body}) : super(key: key);
@@ -12,7 +13,28 @@ class AppScaffold extends StatelessWidget {
         message: "Alpha",
         color: ThemeColors.red,
         child: Scaffold(
-          body: body,
+          body: Stack(
+            children: [
+              body,
+              FutureBuilder(
+                future: _getVersionNumber(),
+                builder: ((context, AsyncSnapshot<String> snapshot) => Positioned(
+                      bottom: 0,
+                      child: Padding(
+                        padding: const EdgeInsets.all(4),
+                        child: Text(
+                          "Version: ${snapshot.data}",
+                          style: const TextStyle(
+                            color: ThemeColors.white25,
+                          ),
+                        ),
+                      ),
+                    )),
+              ),
+            ],
+          ),
         ),
       );
+
+  Future<String> _getVersionNumber() async => (await PackageInfo.fromPlatform()).version;
 }
